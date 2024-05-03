64b2f835b3001774aaa4ee884e986082332892d3
zhucheng <zhucheng@sparkedu.com>
2024-05-03 21:51:56 +0800

    fix: fix gpt-helper

--- gpt-helper/chrome-plugin/ct.js
+++ gpt-helper/chrome-plugin/ct.js
@@ -1,13 +1,12 @@
 
-async function inj_js_ct(){
-  // var scriptContent = `console.log('inj_js_ct>>init')`
-  let f_js=`./gpt/assets/index.js`
-  //eg>>chrome-extension://hienomobjhfbdclmdnihmpppamckjopl/./gpt/assets/index.js
-  let tmp=chrome.runtime.getURL(f_js)
-  var scriptContent = await (await fetch(tmp)).text();
+//eg>>chrome-extension://hienomobjhfbdclmdnihmpppamckjopl/./gpt/assets/index.js
+let f_js=`./gpt/assets/index.js`
+
+function inj_gpt(){
   var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.textContent = scriptContent;
-  (document.head || document.documentElement).appendChild(script);
+  script.src = chrome.runtime.getURL(f_js);
+  document.head.appendChild(script);
 }
-inj_js_ct();
+
+inj_gpt()
+
