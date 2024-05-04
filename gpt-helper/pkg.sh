02616e0f51b513b70146379600a94b8573f367c5
zhucheng <zhucheng@sparkedu.com>
2024-05-04 12:21:11 +0800

    fix: fix pkg.sh

--- gpt-helper/pkg.sh
+++ gpt-helper/pkg.sh
@@ -1,1 +1,4 @@
-zip -j publish/gpt-helper.zip chrome-plugin.crx chrome-plugin/manifest.json
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=./chrome-plugin --pack-extension-key=./publish/gpt_helper.pem
+timestamp=$(date +"%Y%m%d-%H%M%S")
+zip -j "publish/gpt-helper_$timestamp.zip" chrome-plugin.crx chrome-plugin/manifest.json
+rm ./chrome-plugin.crx
