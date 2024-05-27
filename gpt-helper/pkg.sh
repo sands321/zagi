d6deab70a72cfc3e4b1b115deef08b95af2dd704
zhucheng <zhucheng@sparkedu.com>
2024-05-27 21:54:02 +0800

    fix: fix chrome plugin

--- gpt-helper/pkg.sh
+++ gpt-helper/pkg.sh
@@ -1,4 +1,7 @@
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=./chrome-plugin --pack-extension-key=./publish/gpt_helper.pem
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=./chrome-plugin --pack-extension-key=./publish/gpt_helper.pem
+# timestamp=$(date +"%Y%m%d-%H%M%S")
+# zip -j "publish/gpt-helper_$timestamp.zip" chrome-plugin.crx chrome-plugin/manifest.json
+# rm ./chrome-plugin.crx
+#---
 timestamp=$(date +"%Y%m%d-%H%M%S")
-zip -j "publish/gpt-helper_$timestamp.zip" chrome-plugin.crx chrome-plugin/manifest.json
-rm ./chrome-plugin.crx
+zip -r "publish/gpt-helper_$timestamp.zip" chrome-plugin
